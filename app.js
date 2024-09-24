a44699fbf36419eb8d079d0a480e83c92f3e93c4
antish24 <aantenehh@gmail.com>
2024-09-24 07:32:48 -0700

    2nd 99percent 4

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ const PORT=config.PORT
 app.use(cors())
 app.use(express.json());
 
-db.connect(error => {
-    if(error) {
-      console.error('Error connecting to MySQL: ', error);
-      return;
-    }
+// db.connect(error => {
+//     if(error) {
+//       console.error('Error connecting to MySQL: ', error);
+//       return;
+//     }
   
-    console.log('MySQL connected!');
-  });
+//     console.log('MySQL connected!');
+//   });
   
-  db.on('error', error => {
-    console.error('MySQL error: ', error);
-  });
+//   db.on('error', error => {
+//     console.error('MySQL error: ', error);
+//   });
 
 app.use('/',routers)
 app.use('/upload', express.static(path.join(__dirname, '/upload')));
